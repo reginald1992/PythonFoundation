69bfb060c4b8a8bd4e01704f7ba8a82e7b983fc1
Shulin <shulinliu@aliyun.com>
2017-08-14 17:04:50 +0800

    循环

---
+++ PythonBasis/Looping.py
@@ -0,0 +1,38 @@
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+
+sum = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    sum = sum + x
+print(sum)
+
+list(range(100))
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+
+# 请利用循环依次对list中的每个名字打印出Hello, xxx!
+for name in names:
+    print('Hello,', name)
+# 在循环中，break语句可以提前退出循环。
+n = 1
+while n <= 100:
+    if n > 10:  # 当n = 11时，条件满足，执行break语句
+        break  # break语句会结束当前循环
+    print(n)
+    n = n + 1
+print('END')
+# 在循环过程中，也可以通过continue语句，跳过当前的这次循环，直接开始下一次循环。
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0:  # 如果n是偶数，执行continue语句
+        continue  # continue语句会直接继续下一轮循环，后续的print()语句不会执行
+    print(n)
+    # 循环是让计算机做重复任务的有效的方法。
+    # break语句可以在循环过程中直接退出循环，而continue语句可以提前结束本轮循环，并直接开始下一轮循环。这两个语句通常都必须配合if语句使用。
+    # 要特别注意，不要滥用break和continue语句。break和continue会造成代码执行逻辑分叉过多，容易出错。
+    # 大多数循环并不需要用到break和continue语句，上面的两个例子，都可以通过改写循环条件或者修改循环逻辑，去掉break和continue语句。
+    # 有些时候，如果代码写得有问题，会让程序陷入“死循环”，也就是永远循环下去。这时可以用Ctrl+C退出程序，或者强制结束Python进程。
