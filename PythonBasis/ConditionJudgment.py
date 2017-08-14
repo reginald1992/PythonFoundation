d0555067620f206c019aac933ea1cedf239f2443
Shulin <shulinliu@aliyun.com>
2017-08-14 16:37:08 +0800

    条件判断

---
+++ PythonBasis/ConditionJudgment.py
@@ -0,0 +1,37 @@
+s = input('age:')
+age = int(s)
+if age > 18:
+    print('Your age is ', age)
+    print('adult')
+else:
+    print('Your age is ', age)
+    print('teenager')
+b = input('birth: ')
+birth = int(b)
+if birth < 2000:
+    print('00前')
+else:
+    print('00后')
+    # practice
+    # 小明身高1.75，体重80.5kg。请根据BMI公式（体重除以身高的平方）帮小明计算他的BMI指数，并根据BMI指数：
+    # 低于18.5：过轻
+    # 18.5-25：正常
+    # 25-28：过重
+    # 28-32：肥胖
+    # 高于32：严重肥胖
+    # 用if-elif判断并打印结果
+h = input('height:')
+height = float(h)
+w = input('weight:')
+weight = float(w)
+bmi = weight / (height ^ 2)
+if bmi > 32:
+    print('严重肥胖')
+elif bmi > 28:
+    print('肥胖')
+elif bmi > 25:
+    print('过重')
+elif bmi > 18.5:
+    print('正常')
+else:
+    print('过轻')
--- PythonBasis/PythonList.py
+++ PythonBasis/PythonList.py
@@ -1,4 +1,4 @@
-classmates = ['David', 'Bob', 'Tracy']
+classmates = ['David', 'Bob', 'Tracy']  # list的使用
 print(classmates)
 len(classmates)
 print(len(classmates))
@@ -15,3 +15,8 @@ L = [True, 'James', 123, 15.263]
 print(L)
 s = [12, 'sdf sd', 26.33, [12, 26, 29], False]
 print(s)
+J = []
+print(len(J))
+classmates = ('Lucy', 'Jess', 'Jones', 'Tiger')  # tuple和list的不同之处在于tuple一旦初始化就不能做任何修改
+# tuple所谓的“不变”是说，tuple的每个元素，指向永远不变。如果在tuple里面包含一个list，那么list中的元素可以变
+print(classmates)
