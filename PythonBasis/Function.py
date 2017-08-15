0851432ab52404ac001bb5f9bdc049340776bc1d
Shulin <shulinliu@aliyun.com>
2017-08-15 17:01:54 +0800

    函数

--- PythonBasis/Function.py
+++ PythonBasis/Function.py
@@ -59,3 +59,13 @@ print(r)
 # 原来返回值是一个tuple！但是，在语法上，返回一个tuple可以省略括号，而多个变量可以同时接收一个tuple，按位置赋给对应的值，
 # 所以，Python的函数返回多值其实就是返回一个tuple，但写起来更方便。
 
+# practice
+# 请定义一个函数quadratic(a, b, c)，接收3个参数，返回一元二次方程：
+# ax2 + bx + c = 0
+# 的两个解。
+
+
+def quadratic(a, b, c):
+    pass
+
+
