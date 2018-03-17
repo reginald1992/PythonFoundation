d6933a3b657c65d679be80c3ba24fc20501fb06f
LIUSHULIN\shuli <shulinliu@aliyun.com>
2018-03-17 21:12:58 +0800

    高级特性之四：生成器

--- PythonBasis/AdvancedFeatures.py
+++ PythonBasis/AdvancedFeatures.py
@@ -153,3 +153,116 @@ else:
 L = [x * x for x in range(10)]
 g = (x * x for x in range(10))
 # 创建L和g的区别仅在于最外层的[]和()，L是一个list，而g是一个generator。
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+# next()函数获得generator的下一个返回值
+for n in g:
+    print(n)
+# 用generator生成斐波拉契数列：第三个数开始，任意一个数可以又前2个数相加得到
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a+b
+        n = n + 1
+    return 'done'
+
+
+fib(8)
+# fib函数实际上是定义了斐波拉契数列的推算规则，从第一个元素开始，推算出后续任意的元素，这种逻辑其实非常类似generator。
+# 也就是说，上面的函数和generator仅一步之遥。要把fib函数变成generator，只需要把print(b)改为yield b就可以了：
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        a, b = b, a+b
+        n = n + 1
+    return 'done'
+
+
+# 这就是定义generator的另一种方法。
+# 如果一个函数定义中包含yield关键字，那么这个函数就不再是一个普通函数，而是一个generator：
+f = fib(8)
+# generator和函数的执行流程不一样。函数是顺序执行，遇到return语句或者最后一行函数语句就返回。
+# 而变成generator的函数，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+# 请注意区分普通函数和generator函数，普通函数调用直接返回结果;generator函数返回一个generator对象
+
+
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield(3)
+    print('step 3')
+    yield(5)
+
+
+o = odd()
+next(o)
+next(o)
+next(o)
+# next(o)
+# odd不是普通函数，而是generator，在执行过程中，遇到yield就中断，下次又继续执行。
+# 执行3次yield后，已经没有yield可以执行了，所以，第4次调用next(o)就报错。
+g = fib(6)
+# while True:
+#     try:
+#         x = next(g)
+#         print('g:', x)
+#     except StopIteration as e:
+#         print('Generator return value:', e.value)
+#         break
+
+# 练习
+# 把每一行看做一个list，试写一个generator，不断输出下一行的list：
+
+
+def triangles():
+    L = [1]
+    while 1:
+        yield L
+        L = [1] + [L[i] + L[i + 1] for i in range(len(L) - 1)] + [1]
+
+
+# 期待输出:
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# [1, 5, 10, 10, 5, 1]
+# [1, 6, 15, 20, 15, 6, 1]
+# [1, 7, 21, 35, 35, 21, 7, 1]
+# [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+
+
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
